7d1d0d8a278f3d415bb9184da97a6ae1998be47a
Mohammed Alshahrani <mmogib@gmail.com>
2024-01-26 11:17:17 +0300

    fea: first fun function

--- src/UIDFPA.jl
+++ src/UIDFPA.jl
@@ -2,4 +2,14 @@ module UIDFPA
 
 # Write your package code here.
 
+"""@docs
+    firstfun(a,b)
+returns a + b
+"""
+function firstfun(a, b)
+    a + b + 1
+end
+
+export firstfun
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,7 @@ using Test
 
 @testset "UIDFPA.jl" begin
     # Write your tests here.
+
+    @test 3 == firstfun(1, 2)
+    @test 1 == firstfun(1, 2)
 end
