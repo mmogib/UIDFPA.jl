b12c9cc0151d52b5a561af5a10a8c37071199ef3
Documenter.jl <documenter@juliadocs.github.io>
2024-01-26 08:28:39 +0000

    build based on 5d3e089

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = UIDFPA","category":"page"},{"location":"#UIDFPA","page":"Home","title":"UIDFPA","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for UIDFPA.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [UIDFPA]","category":"page"},{"location":"#UIDFPA.firstfun-Tuple{Any, Any}","page":"Home","title":"UIDFPA.firstfun","text":"@docs     firstfun(a,b) returns a + b\n\n\n\n\n\n","category":"method"}]
+}
