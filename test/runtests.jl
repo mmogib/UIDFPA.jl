942ea8ea4a3ce4359617f97e5069e2c87f02d85b
Mohammed Alshahrani <mmogib@gmail.com>
2024-03-03 08:16:02 +0300

    fix: testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,17 @@ using Test
 
 @testset "UIDFPA.jl" begin
     # Write your tests here.
-
-    @test 3 == firstfun(1, 2)
-    @test -3 == firstfun(-5, 2)
+    n = 1_000
+    F(x) = exp.(x) .- 1
+    A = ones(1, n)
+    b = [n]
+    lb = -ones(n)
+    ub = n * ones(n)
+    Omega = Polyhedral(A, b, lb, ub)
+    problem = NECProblem(F, Omega)
+    params = UIDFPAParams(0.25, 0.5)
+    x0 = zeros(n)
+    solution = uidfpa(problem, x0, params, ϵ=1e-6, mxitrs=2000)
+    @test solution.solution.Fnorm <= 1e-6
+    @test solution.solution.x ≈ zeros(n)
 end
